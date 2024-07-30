498e9d123a295679a1f5433c0f07bf87ce0d7be7
Thales Mariano <thales.mariano125@gmail.com>
2024-07-30 09:20:41 -0300

    dotenv added

--- App/Models/Conexao.php
+++ App/Models/Conexao.php
@@ -1,6 +1,10 @@
 <?php 
 
 namespace App\Models;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 class Conexao {
 	private static $conn;
@@ -8,7 +12,10 @@ class Conexao {
 	public static function getConn(){
 		
 		if(!isset(self::$conn)):
-			self::$conn = new \PDO('mysql:host=localhost;dbname=rede_social_foda;charset=utf8', 'root', '');
+			self::$conn = new \PDO(
+				$_ENV['DB_DRIVER'].':host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_NAME'].';charset=utf8',
+				$_ENV['DB_USER'],
+				$_ENV['DB_PASS']);
 		endif;
 
 		return self::$conn;
